e43d77065640f29be5573a7c5d92f05179ae71e0
JohnnyNWT <jooaaooskt@gmail.com>
2022-07-27 22:30:55 -0300

    Exercicio - 9

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise8.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise8.js
@@ -6,3 +6,13 @@ const greet = (name, msg = 'Hi') => `${msg} ${name}`;
 console.log(greet('John')) // 'Hi John'
 console.log(greet('John', 'Good morning')) // 'Good morning John'
 console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
+
+const yearSeasons = {
+    spring: ['March', 'April', 'May'],
+    summer: ['June', 'July', 'August'],
+    autumn: ['September', 'October', 'November'],
+    winter: ['December', 'January', 'February'],
+  };
+  
+  const { spring, summer, autumn, winter } = yearSeasons;
+  console.log(yearSeasons)
